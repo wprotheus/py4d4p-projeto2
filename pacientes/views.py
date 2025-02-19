56fa212ab9fcee56051c2154283239d3a5a017a6
wprotheus <wellneto@hotmail.com>
2025-02-18 21:03:23 -0300

    Pythonando 4Days4Projects - Projeto 2.

---
+++ pacientes/views.py
@@ -0,0 +1,97 @@
+from django.http import Http404
+from django.shortcuts import redirect, render
+from .models import Consultas, Pacientes, Tarefas, Visualizacoes
+from django.contrib import messages
+from django.contrib.messages import constants
+
+
+def pacientes(request):
+    if request.method == "GET":
+        pacientes_list = Pacientes.objects.all()
+        return render(request, 'pacientes.html', {'queixas': Pacientes.queixa_choices, 'pacientes': pacientes_list})
+    else:
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        queixa = request.POST.get('queixa')
+        foto = request.FILES.get('foto')
+
+        if len(nome.strip()) == 0 or not foto:
+            messages.add_message(request, constants.ERROR, 'O campo nome e foto são obrigatórios')
+            return redirect('pacientes')
+
+        paciente = Pacientes(
+            nome=nome,
+            email=email,
+            telefone=telefone,
+            queixa=queixa,
+            foto=foto
+        )
+        paciente.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Paciente adicionado com sucesso')
+        return redirect('pacientes')
+    
+
+def paciente_view(request, id):
+    paciente = Pacientes.objects.get(id=id)
+    if request.method == "GET":
+        tarefas = Tarefas.objects.all()
+        consultas = Consultas.objects.filter(paciente=paciente)
+
+        tuple_grafico = ([str(i.data) for i in consultas], [str(i.humor) for i in consultas])
+
+        return render(request, 'paciente.html', {'tarefas': tarefas, 'paciente': paciente, 'consultas': consultas, 'tuple_grafico': tuple_grafico})
+    else:
+        humor = request.POST.get('humor')
+        registro_geral = request.POST.get('registro_geral')
+        video = request.FILES.get('video')
+        tarefas = request.POST.getlist('tarefas')
+
+        consultas = Consultas(
+            humor=int(humor),
+            registro_geral=registro_geral,
+            video=video,
+            paciente=paciente
+        )
+        consultas.save()
+
+        for i in tarefas:
+            tarefa = Tarefas.objects.get(id=i)
+            consultas.tarefas.add(tarefa)
+
+        consultas.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Registro de consulta adicionado com sucesso.')
+        return redirect(f'/pacientes/{id}')
+
+def atualizar_paciente(request, id):
+    paciente = Pacientes.objects.get(id=id)
+    pagamento_em_dia = request.POST.get('pagamento_em_dia')
+    status = True if pagamento_em_dia == 'ativo' else False
+    paciente.pagamento_em_dia = status
+    paciente.save()
+    return redirect(f'/pacientes/{id}')
+
+def excluir_consulta(request, id):
+    consulta = Consultas.objects.get(id=id)
+    consulta.delete()
+    return redirect(f'/pacientes/{consulta.paciente.id}')
+
+def consulta_publica(request, id):
+    consulta = Consultas.objects.get(id=id)
+    if not consulta.paciente.pagamento_em_dia:
+        raise Http404()
+    '''view = Visualizacoes(
+    consulta=consulta,
+    ip=request.META['REMOTE_ADDR']
+    )
+    view.save()'''
+    return render(request, 'consulta_publica.html', {'consulta': consulta})
+
+@property
+def views(self):
+    views = Visualizacoes.objects.filter(consulta=self)
+    totais = views.count()
+    unicas = views.values('ip').distinct().count()    
+    return f'{totais} - {unicas}'
